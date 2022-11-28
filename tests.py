67b26b4daab188b2990a8c8eacab674aa52d831b
Vladimir Dorofeev <daskodex@gmail.com>
2022-11-28 19:56:22 +0600

    add 4 new test

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ from main import check_valid_func_names
 import pytest
 
 def test_run():
+    assert check_valid_func_names('students_works/test_student_work_0.py') == 1
     assert check_valid_func_names('students_works/test_student_work_1.py') == 1
-    # assert check_valid_func_names('students_works\test_student_work_2.py') == 1
-    # assert check_valid_func_names('students_works\test_student_work_3.py') == 1
-    # assert check_valid_func_names('students_works\test_student_work_ok.py') == 0
+    assert check_valid_func_names('students_works/test_student_work_2.py') == 1
+    assert check_valid_func_names('students_works/test_student_work_ok.py') == 0
