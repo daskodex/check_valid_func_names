c02c22de7f7eca8e7575a9e5efdf34d01d73d3cc
Vladimir Dorofeev <daskodex@gmail.com>
2022-11-28 19:53:48 +0600

    new tests added

--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
 from main import check_valid_func_names
 import pytest
 
-def test_for_check_valid_func_names:
-    assert check_valid_func_names('tests/test_student_work_1.py')   == 1, "wrong function names, test ok"
+def test_run():
+    assert check_valid_func_names('students_works/test_student_work_1.py') == 1
+    # assert check_valid_func_names('students_works\test_student_work_2.py') == 1
+    # assert check_valid_func_names('students_works\test_student_work_3.py') == 1
+    # assert check_valid_func_names('students_works\test_student_work_ok.py') == 0
