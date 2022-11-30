5fbef7e369ee331c28debb57141f943d9d8bcbc5
Vladimir Dorofeev <daskodex@gmail.com>
2022-11-30 22:23:41 +0600

    new test file

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
-from main import check_valid_func_names
+from main import check_valid_functions_names
 import pytest
 
 def test_run():
-    assert check_valid_func_names('students_works/test_student_work_0.py') == 1
-    assert check_valid_func_names('students_works/test_student_work_1.py') == 1
-    assert check_valid_func_names('students_works/test_student_work_2.py') == 1
-    assert check_valid_func_names('students_works/test_student_work_ok.py') == 0
+    assert check_valid_functions_names('students_works/test_student_work_0.py') == 1
+    assert check_valid_functions_names('students_works/test_student_work_1.py') == 1
+    assert check_valid_functions_names('students_works/test_student_work_2.py') == 1
+    assert check_valid_functions_names('students_works/test_student_work_ok.py') == 0
+    assert check_valid_functions_names('students_works/test_student_work_5.py') == 1
