30cc00535ce1cf97bd6310a52ea64f45b7392300
Vladimir Dorofeev <daskodex@gmail.com>
2022-12-01 17:15:48 +0600

    add syntax error example

---
+++ students_works/test_student_work_6_syntax_error.py
@@ -0,0 +1,25 @@
+#Пример кода где Правило выполняется
+def CircleFactory():
+    print('hello')
+    print('world')
+
+class Triangle():
+    def method_one(self):
+        pass
+
+    def method_two(self):
+        pass
+
+
+
+#Пример кода, где Правило не выполняется
+def TriangleFactory():
+    print('hello')
+    print('world')
+
+
+clas Round():
+    def method_one(self):
+        pass
+    def method_two(self):
+        pass
