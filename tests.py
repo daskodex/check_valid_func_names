e5900154f5b13d7bbc68d8f5874ba518bbfdc4a0
Vladimir Dorofeev <daskodex@gmail.com>
2022-12-01 17:20:44 +0600

    add new test (6), syntax errors

--- tests.py
+++ tests.py
@@ -7,3 +7,4 @@ def test_run():
     assert check_valid_functions_names('students_works/test_student_work_2.py') == 1
     assert check_valid_functions_names('students_works/test_student_work_ok.py') == 0
     assert check_valid_functions_names('students_works/test_student_work_5.py') == 1
+    assert check_valid_functions_names('students_works/test_student_work_6_syntax_error.py') == 1
